f33d9ffe719d9050edef5e4d62158ba1623cc599
Nigel Horne <njh@bandsman.co.uk>
2024-12-19 16:53:24 -0500

    Not all sections were being displayed

--- lib/PDS/DB/sections.pm
+++ lib/PDS/DB/sections.pm
@@ -6,4 +6,13 @@ use Database::Abstraction;
 
 our @ISA = ('Database::Abstraction');
 
+# The entry column is not unique, so don't slurp it
+sub new
+{
+	my $class = shift;
+	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	return $class->SUPER::new(max_slurp_size => 0, %args);
+}
+
 1;
