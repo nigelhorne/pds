6c07198bcd5168a4fd893050c728166bdcdd0f9d
Nigel Horne <njh@bandsman.co.uk>
2024-12-21 14:18:07 -0500

    Sync Display.pm with VWF

--- lib/PDS/Display.pm
+++ lib/PDS/Display.pm
@@ -5,25 +5,60 @@ package PDS::Display;
 
 use strict;
 use warnings;
-use PDS::Config;
-use PDS::Allow;
+
+use Config::Auto;
 use CGI::Info;
+use Data::Dumper;
 use File::Spec;
 use Template::Filters;
 use Template::Plugin::EnvHash;
+use Template::Plugin::Math;
 use HTML::SocialMedia;
+use PDS::Utils;
 use Error;
 use Fatal qw(:void open);
 use File::pfopen;
+use Scalar::Util;
+
+# TODO: read this from the config file
+my %blacklist = (
+	'MD' => 1,
+	'RU' => 1,
+	'CN' => 1,
+	'BR' => 1,
+	'UY' => 1,
+	'TR' => 1,
+	'MA' => 1,
+	'VE' => 1,
+	'SA' => 1,
+	'CY' => 1,
+	'CO' => 1,
+	'MX' => 1,
+	'IN' => 1,
+	'RS' => 1,
+	'PK' => 1,
+);
 
 our $sm;
 our $smcache;
 
 sub new {
-	my $proto = shift;
+	my $class = shift;
+
+	# Handle hash or hashref arguments
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
-	my $class = ref($proto) || $proto;
+	if(!defined($class)) {
+		# Using PDS::Display->new(), not PDS::Display::new()
+		# carp(__PACKAGE__, ' use ->new() not ::new() to instantiate');
+		# return;
+
+		# FIXME: this only works when no arguments are given
+		$class = __PACKAGE__;
+	} elsif(Scalar::Util::blessed($class)) {
+		# If $class is an object, clone it with new arguments
+		return bless { %{$class}, %args }, ref($class);
+	}
 
 	if(defined($ENV{'HTTP_REFERER'})) {
 		# Protect against Shellshocker
@@ -36,20 +71,68 @@ sub new {
 	}
 
 	my $info = $args{info} || CGI::Info->new();
-	my $config = $args{config} || PDS::Config->new({ logger => $args{logger}, info => $info, lingua => $args{lingua} });
 
 	unless($info->is_search_engine() || !defined($ENV{'REMOTE_ADDR'})) {
-		my %allowargs = (
-			info => $info,
-			config => $config,
-			lingua => $args{lingua},
-			logger => $args{logger},
-			cachedir => $args{cachedir},
-			cache => $args{cache}
-		);
-		unless(PDS::Allow::allow(%allowargs)) {
-			throw Error::Simple("Not allowing connexion from $ENV{'REMOTE_ADDR'}", 1);
+		require CGI::IDS;
+		CGI::IDS->import();
+
+		my $ids = CGI::IDS->new();
+		$ids->set_scan_keys(scan_keys => 1);
+		my $impact = $ids->detect_attacks(request => $info->params());
+		if($impact > 0) {
+			die "IDS impact is $impact";
 		}
+
+		require Data::Throttler;
+		Data::Throttler->import();
+
+		# Handle YAML Errors
+		my $db_file = File::Spec->catdir($info->tmpdir(), 'throttle');
+		eval {
+			my $throttler = Data::Throttler->new(
+				max_items => 30,
+				interval => 90,
+				backend => 'YAML',
+				backend_options => {
+					db_file => $db_file
+				}
+			);
+
+			unless($throttler->try_push(key => $ENV{'REMOTE_ADDR'})) {
+				die "$ENV{REMOTE_ADDR} connexion throttled";
+			}
+		};
+		if($@) {
+			unlink($db_file);
+		}
+		if(my $lingua = $args{lingua}) {
+			if($blacklist{uc($lingua->country())}) {
+				die "$ENV{REMOTE_ADDR} is from a blacklisted country ", $lingua->country();
+			}
+		}
+	}
+	my $config_dir = _find_config_dir(\%args, $info);
+	if($args{'logger'}) {
+		$args{'logger'}->debug(__PACKAGE__, ': ', __LINE__, " path = $config_dir");
+	}
+	my $config;
+	eval {
+		if(-r File::Spec->catdir($config_dir, $info->domain_name())) {
+			$config = Config::Auto::parse($info->domain_name(), path => $config_dir);
+		} elsif (-r File::Spec->catdir($config_dir, 'default')) {
+			$config = Config::Auto::parse('default', path => $config_dir);
+		} else {
+			die 'no suitable config file found';
+		}
+	};
+	if($@ || !defined($config)) {
+		die "Configuration error: $@: $config_dir/", $info->domain_name();
+	}
+
+	# The values in config are defaults which can be overridden by
+	# the values in args{config}
+	if(defined($args{'config'})) {
+		$config = { %{$config}, %{$args{'config'}} };
 	}
 
 	Template::Filters->use_html_entities();
@@ -57,14 +140,19 @@ sub new {
 	my $self = {
 		_config => $config,
 		_info => $info,
-		_lingua => $args{lingua},
 		_logger => $args{logger},
 		_cachedir => $args{cachedir},
-		# _page => $info->param('page'),
+		%args,
 	};
 
+	if(my $lingua = $args{'lingua'}) {
+		$self->{'_lingua'} = $lingua;
+	}
 	if(my $key = $info->param('key')) {
 		$self->{'_key'} = $key;
+	}
+	if(my $page = $info->param('page')) {
+		$self->{'_page'} = $page;
 	}
 
 	if(my $twitter = $config->{'twitter'}) {
@@ -78,7 +166,64 @@ sub new {
 	$self->{'_social_media'}->{'facebook_share_button'} = $sm->as_string(facebook_share_button => 1);
 	# $self->{'_social_media'}->{'google_plusone'} = $sm->as_string(google_plusone => 1);
 
+	# Return the blessed object
 	return bless $self, $class;
+}
+
+# Determine the configuration directory
+sub _find_config_dir
+{
+	my($args, $info) = @_;
+
+	if($ENV{'CONFIG_DIR'}) {
+		return $ENV{'CONFIG_DIR'};
+	}
+
+	my $config_dir = File::Spec->catdir(
+			$info->script_dir(),
+			File::Spec->updir(),
+			File::Spec->updir(),
+			'conf'
+		);
+
+	if(!-d $config_dir) {
+		$config_dir = File::Spec->catdir(
+				$info->script_dir(),
+				File::Spec->updir(),
+				'conf'
+			);
+	}
+
+	if(!-d $config_dir) {
+		if($ENV{'DOCUMENT_ROOT'}) {
+			$config_dir = File::Spec->catdir(
+				# $ENV{'DOCUMENT_ROOT'},
+				$info->rootdir(),
+				File::Spec->updir(),
+				'lib',
+				'conf'
+			);
+		} else {
+			$config_dir = File::Spec->catdir(
+				$ENV{'HOME'},
+				'lib',
+				'conf'
+			);
+		}
+	}
+
+	if(!-d $config_dir) {
+		if($args->{config_directory}) {
+			return $args->{config_directory};
+		}
+		if($args->{logger}) {
+			while(my ($k, $v) = each %ENV) {
+				$args->{logger}->debug("$k=$v");
+			}
+		}
+	}
+
+	return $config_dir;
 }
 
 # Call this to display the page
@@ -89,12 +234,13 @@ sub as_string {
 	# TODO: Get all cookies and send them to the template.
 	# 'cart' is an example
 	unless($args && $args->{cart}) {
-		my $purchases = $self->{_info}->get_cookie(cookie_name => 'cart');
-		if($purchases) {
+		if(my $purchases = $self->{_info}->get_cookie(cookie_name => 'cart')) {
 			my %cart = split(/:/, $purchases);
 			$args->{cart} = \%cart;
 		}
 	}
+
+	# Calculate items in cart if not already present in $args
 	unless($args && $args->{itemsincart}) {
 		if($args->{cart}) {
 			my $itemsincart;
@@ -120,9 +266,15 @@ sub as_string {
 	return $rc =~ /^Location:\s/ms ? $rc : $rc . $self->html($args);
 }
 
-sub get_template_path {
+# Determine the path to the correct template file based on various criteria such as language settings, browser type, and module path
+sub get_template_path
+{
 	my $self = shift;
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	if($self->{_logger}) {
+		$self->{_logger}->trace('Entering get_template_path');
+	}
 
 	if($self->{_filename}) {
 		return $self->{_filename};
@@ -130,41 +282,32 @@ sub get_template_path {
 
 	my $dir = $self->{_config}->{root_dir} || $self->{_info}->root_dir();
 	if($self->{_logger}) {
-		$self->{_logger}->debug("Rootdir: $dir");
+		$self->{_logger}->debug(__PACKAGE__, ': ', __LINE__, ": root_dir $dir");
+		$self->{_logger}->debug(Data::Dumper->new([$self->{_config}])->Dump());
 	}
 	$dir .= '/templates';
 
+	my $prefix;
+
 	# Look in .../robot or .../mobile first, if appropriate
-	my $prefix = '';
-
 	# Look in .../en/gb/web, then .../en/web then /web
-	if($self->{_lingua}) {
-		my $lingua = $self->{_lingua};
-
-		$self->_debug({ message => 'Requested language: ' . $lingua->requested_language() });
-
-		my $candidate;
-		if($lingua->sublanguage_code_alpha2()) {
-			$candidate = "$dir/" . $lingua->code_alpha2() . '/' . $lingua->sublanguage_code_alpha2();
-			$self->_debug({ message => "check for directory $candidate" });
-			if(!-d $candidate) {
-				$candidate = undef;
+	foreach my $browser_type($self->_types()) {
+		if(my $lingua = $self->{_lingua}) {
+			$self->_debug({ message => 'Requested language: ' . $lingua->requested_language() });
+			# FIXME: look for lower priority languages if the highest isn't found
+			if(my $language = $lingua->language_code_alpha2()) {
+				if(my $dialect = $lingua->sublanguage_code_alpha2()) {
+					$prefix .= "$dir/$browser_type/$language/$dialect:";
+					$prefix .= "$dir/$browser_type/$language/default:";
+				}
+				$prefix .= "$dir/$language/$browser_type:" if(-d "$dir/$language/$browser_type");
+				$prefix .= "$dir/$browser_type/$language:" if(-d "$dir/$browser_type/$language");
+				$prefix .= "$dir/$browser_type/default:" if(-d "$dir/$browser_type/default");
+				$prefix .= "$dir/default/$browser_type/:" if(-d "$dir/default/$browser_type");
 			}
 		}
-		if((!defined($candidate)) && defined($lingua->code_alpha2())) {
-			$candidate = "$dir/" . $lingua->code_alpha2();
-			$self->_debug({ message => "check for directory $candidate" });
-			if(!-d $candidate) {
-				$candidate = undef;
-			}
-		}
-		if($candidate) {
-			$prefix = $self->_append_browser_type({ directory => $candidate });
-		}
+		$prefix .= "$dir/$browser_type:" if(-d "$dir/$browser_type");
 	}
-
-	$prefix .= $self->_append_browser_type({ directory => "$dir/default" });
-	$prefix .= $self->_append_browser_type({ directory => $dir });
 
 	# Fall back to .../web, or if that fails, assume no web, robot or
 	# mobile variant
@@ -179,7 +322,7 @@ sub get_template_path {
 		throw Error::Simple("Prefix must not contain ../ ($prefix)");
 	}
 
-        # Untaint the prefix value which may have been read in from a configuration file
+	# Untaint the prefix value which may have been read in from a configuration file
 	($prefix) = ($prefix =~ m/^([A-Z0-9_\.\-\/:]+)$/ig);
 
 	my ($fh, $filename) = File::pfopen::pfopen($prefix, $modulepath, 'tmpl:tt:html:htm:txt');
@@ -189,6 +332,12 @@ sub get_template_path {
 	close($fh);
 	$self->_debug({ message => "using $filename" });
 	$self->{_filename} = $filename;
+
+	# Remember the template filename
+	if($self->{'log'}) {
+		$self->{'log'}->template($filename);
+	}
+
 	return $filename;
 }
 
@@ -200,7 +349,6 @@ sub set_cookie
 	foreach my $key(keys(%params)) {
 		$self->{_cookies}->{$key} = $params{$key};
 	}
-
 	return $self;
 }
 
@@ -218,7 +366,7 @@ sub http
 	}
 
 	# Determine language, defaulting to English
-	# TODO: Change the headers, e.g. character set, based on the langauge
+	# TODO: Change the headers, e.g. character set, based on the language
 	# my $language = $self->{_lingua} ? $self->{_lingua}->language() : 'English';
 
 	# Determine content type
@@ -234,11 +382,15 @@ sub http
 	# Security headers
 	# https://www.owasp.org/index.php/Clickjacking_Defense_Cheat_Sheet
 	# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+
+	# TODO: investigate Content-Security-Policy
 	return $rc . "X-Frame-Options: SAMEORIGIN\n"
 		. "X-Content-Type-Options: nosniff\n"
 		. "Referrer-Policy: strict-origin-when-cross-origin\n\n";
 }
 
+# Override this routine in a subclass if you wish to create special arguments to
+# send to the template
 sub html {
 	my $self = shift;
 	my %params = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
@@ -248,12 +400,6 @@ sub html {
 	if($filename =~ /.+\.t(mpl|t)$/) {
 		require Template;
 		Template->import();
-
-		my $template = Template->new({
-			INTERPOLATE => 1,
-			POST_CHOMP => 1,
-			ABSOLUTE => 1,
-		});
 
 		my $info = $self->{_info};
 
@@ -282,14 +428,20 @@ sub html {
 		$vals->{info} = $info;
 		$vals->{as_string} = $info->as_string();
 
+		my $template = Template->new({
+			INTERPOLATE => 1,
+			POST_CHOMP => 1,
+			ABSOLUTE => 1,
+		});
+
 		if(!$template->process($filename, $vals, \$rc)) {
 			if(my $err = $template->error()) {
-                                throw Error::Simple($err);
-                        }
-                        throw Error::Simple("Unknown error in template: $filename");
-                }
+				throw Error::Simple($err);
+			}
+			throw Error::Simple("Unknown error in template: $filename");
+		}
 	} elsif($filename =~ /\.(html?|txt)$/) {
-		open(my $fin, '<', $filename) || die "$filename: $!";
+		open(my $fin, '<', $filename) || throw Error::Simple("$filename: $!");
 
 		my @lines = <$fin>;
 
@@ -300,10 +452,8 @@ sub html {
 		throw Error::Simple("Unhandled file type $filename");
 	}
 
-	if(($filename !~ /.txt$/) && ($rc =~ /\smailto:(.+?)>/)) {
-		unless($1 =~ /^&/) {
-			$self->_debug({ message => "Found mailto link $1, you should remove it or use " . obfuscate($1) . ' instead' });
-		}
+	if(($filename !~ /.txt$/) && ($rc =~ /\smailto:(.+?)>/) && ($1 !~ /^&/) && $self->{_logger}) {
+		$self->{_logger}->warn({ message => "Found mailto link $1, you should remove it or use " . obfuscate($1) . ' instead' });
 	}
 
 	return $rc;
@@ -313,12 +463,12 @@ sub _debug
 {
 	my $self = shift;
 
-	if($self->{_logger}) {
+	if(my $logger = $self->{_logger}) {
 		my %params = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
-		if($ENV{'REMOTE_ADDR'}) {
-			$self->{_logger}->debug("$ENV{'REMOTE_ADDR'}: $params{'message'}");
+		if(defined($ENV{'REMOTE_ADDR'})) {
+			$logger->debug("$ENV{'REMOTE_ADDR'}: $params{'message'}");
 		} else {
-			$self->{_logger}->debug($params{'message'});
+			$logger->debug($params{'message'});
 		}
 	}
 	return $self;
@@ -328,38 +478,22 @@ sub obfuscate {
 	return map { '&#' . ord($_) . ';' } split(//, shift);
 }
 
-sub _append_browser_type {
+sub _types
+{
 	my $self = shift;
-	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+	my $info = $self->{_info};
+	my @rc;
 
-	if($self->{_logger}) {
-		$self->{_logger}->trace('_append_browser_type');
+	if($info->is_search_engine()) {
+		push @rc, 'search', 'robot';
+	} elsif($info->is_mobile()) {
+		push @rc, 'mobile';
+	} elsif($info->is_robot()) {
+		push @rc, 'robot', 'search';
 	}
+	push @rc, 'web';
 
-	my $directory = $args{'directory'};
-
-	return unless(defined($directory));
-
-	if($self->{_logger}) {
-		$self->{_logger}->debug("_append_browser_type: directory = $directory");
-	}
-
-	my $rc;
-	if(-d $directory) {
-		if($self->{_info}->is_search_engine()) {
-			$rc = "$directory/search:$directory/robot:";
-		} elsif($self->{_info}->is_mobile()) {
-			$rc = "$directory/mobile:";
-		} elsif($self->{_info}->is_robot()) {
-			$rc = "$directory/robot:$directory/search:";
-		}
-		$rc .= "$directory/web:";
-
-		$self->_debug({ message => "_append_directory_type: $directory=>$rc" });
-		return $rc;
-	}
-
-	return '';	# Don't return undef or else the caller may use an uninit variable
+	return @rc;
 }
 
 1;
