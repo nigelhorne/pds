189f139923a917c1298bec7869e988630d8a4258
Nigel Horne <njh@bandsman.co.uk>
2025-08-08 15:51:08 -0400

    Updated Config.pm from VWF

--- lib/PDS/Config.pm
+++ lib/PDS/Config.pm
@@ -1,17 +1,5 @@
 package PDS::Config;
 
-=head1 NAME
-
-PDS::Config - Site independent configuration file
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
 # PDS is licensed under GPL2.0 for personal use only
 # njh@bandsman.co.uk
 
@@ -24,13 +12,34 @@ our $VERSION = '0.01';
 
 use warnings;
 use strict;
-use Config::Auto;
+
+use Carp;
+use Config::Abstraction;
 use CGI::Info;
+use Data::Dumper;
+use Error::Simple;
 use File::Spec;
+use Params::Get 0.13;
+
+=encoding utf-8
+
+=head1 NAME
+
+PDS::Config - Site-independent configuration file for the Versatile Web Framework
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
 
 =head1 SUBROUTINES/METHODS
 
 =head2 new
+
+Creates a new PDS::Config instance with hierarchical configuration loading.
 
 Takes four optional arguments:
 	info (CGI::Info object)
@@ -41,126 +50,205 @@ Takes four optional arguments:
 
 Values in the file are overridden by what's in the environment
 
+B<Parameters:>
+
+=over 4
+
+=item * C<info> - CGI::Info object (optional, created if not provided)
+
+=item * C<logger> - Logger object with debug() method (optional)
+
+=item * C<config_directory> - Additional config directory path (optional)
+
+=item * C<config_file> - Specific config filename (optional)
+
+=item * C<config> - Hash ref of override values (optional)
+
+=back
+
+B<Configuration Resolution Order:>
+1. Base configuration files
+2. Values from config parameter
+3. Environment variable overrides
+
+B<Directory Search Order:>
+1. $ENV{CONFIG_DIR} (if set)
+2. Provided config_directory
+3. ../conf relative to script
+4. ../../conf relative to script
+5. $DOCUMENT_ROOT/../lib/conf
+6. $HOME/lib/conf
+
+B<Returns:> Blessed PDS::Config object
+
+B<Throws:> Error::Simple on configuration errors
+
+=head3 FORMAL SPECIFICATION
+
+    [STRING, HASH, LOGGER]
+
+    ConfigState ::= ⟨⟨ config_dirs : ℙ STRING;
+                      config_data : HASH;
+                      logger : LOGGER ⟩⟩
+
+    ConfigArgs ::= ⟨⟨ info : CGI_Info;
+                     logger : LOGGER;
+                     config_directory : STRING;
+                     config_file : STRING;
+                     config : HASH ⟩⟩
+
+    Init : ConfigArgs → ConfigState
+
+    ∀ params : ConfigArgs •
+      let dirs == if env.CONFIG_DIR ≠ ∅
+                  then {env.CONFIG_DIR}
+                  else default_dirs ∪ {params.config_directory} fi •
+      let valid_dirs == {d : dirs | ∃ f : FILE • readable(d, f)} •
+      valid_dirs ≠ ∅ ∧
+      config_data ∈ HASH ∧
+      config_data = merge(file_config, params.config, env_overrides)
+
+    ValidConfigKey == {k : STRING | k ∈ dom config_data}
+
+    GetConfigValue : ValidConfigKey → (STRING ∪ HASH ∪ ARRAY)
+
 =cut
 
 sub new
 {
 	my $proto = shift;
-	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+	my $params = Params::Get::get_params(undef, @_);
 
-	my $class = ref($proto) || $proto;
-	my $info = $args{info} || CGI::Info->new();
-
-	if($args{'logger'}) {
-		$args{'logger'}->debug(__PACKAGE__, '->new()');
+	if (exists $params->{logger} && defined $params->{logger}) {
+		Throw Error::Simple('logger must be an object with debug method')
+		    unless ref($params->{logger}) && $params->{logger}->can('debug');
 	}
 
-	my $config_dir;
+	if($params->{'logger'}) {
+		$params->{'logger'}->debug(__PACKAGE__, '->new()');
+	}
+
+	if(exists $params->{config} && defined $params->{config}) {
+		Throw Error::Simple('config must be a hash reference') unless ref($params->{config}) eq 'HASH';
+	}
+
+	my $class = ref($proto) || $proto;
+	my $info = $params->{info} || CGI::Info->new();
+
+	my @config_dirs;
 	if($ENV{'CONFIG_DIR'}) {
-		$config_dir = $ENV{'CONFIG_DIR'};
+		# Validate directory exists
+		throw Error::Simple("CONFIG_DIR '$ENV{CONFIG_DIR}' does not exist or is not readable")
+			unless -d $ENV{'CONFIG_DIR'} && -r $ENV{'CONFIG_DIR'};
+		@config_dirs = ($ENV{'CONFIG_DIR'});
 	} else {
-		$config_dir = File::Spec->catdir(
+		if($params->{config_directory}) {
+			throw Error::Simple("config_directory must be a string") if(ref($params->{config_directory}));
+			throw Error::Simple("config_directory '$params->{config_directory}' does not exist")
+				unless -d $params->{config_directory};
+			push(@config_dirs, $params->{config_directory});
+		}
+		@config_dirs = (
+			File::Spec->catdir(
 				$info->script_dir(),
 				File::Spec->updir(),
 				File::Spec->updir(),
 				'conf'
-			);
-		if($args{logger}) {
-			$args{logger}->debug("Looking for configuration $config_dir/", $info->domain_name());
-		}
+			), File::Spec->catdir(
+				$info->script_dir(),
+				File::Spec->updir(),
+				'conf'
+			)
+		);
 
-		if(!-d $config_dir) {
-			$config_dir = File::Spec->catdir(
-					$info->script_dir(),
-					File::Spec->updir(),
-					'conf'
-				);
-			if($args{logger}) {
-				$args{logger}->debug("Looking for configuration $config_dir/", $info->domain_name());
-			}
+		if($ENV{'DOCUMENT_ROOT'}) {
+			push(@config_dirs, File::Spec->catdir(
+				$ENV{'DOCUMENT_ROOT'},
+				File::Spec->updir(),
+				'lib',
+				'conf'
+			))
 		}
-
-		if(!-d $config_dir) {
-			if($ENV{'DOCUMENT_ROOT'}) {
-				$config_dir = File::Spec->catdir(
-					$ENV{'DOCUMENT_ROOT'},
-					File::Spec->updir(),
-					'lib',
-					'conf'
-				);
-			} else {
-				$config_dir = File::Spec->catdir(
-					$ENV{'HOME'},
-					'lib',
-					'conf'
-				);
-			}
-			if($args{logger}) {
-				$args{logger}->debug("Looking for configuration $config_dir/", $info->domain_name());
-			}
+		if($ENV{'HOME'}) {
+			push(@config_dirs, File::Spec->catdir(
+				$ENV{'HOME'},
+				'lib',
+				'conf'
+			));
 		}
+	}
 
-		if(!-d $config_dir) {
-			if($args{config_directory}) {
-				$config_dir = $args{config_directory};
-			} elsif($args{logger}) {
-				while(my ($key,$value) = each %ENV) {
-					$args{logger}->debug("$key=$value");
+	# Look for localised configurations
+	my $language;
+	if(my $lingua = $params->{'lingua'}) {
+		$language = $lingua->language_code_alpha2();
+	}
+	$language ||= $info->lang();
+
+	if($language) {
+		@config_dirs = map {
+			($_, "$_/default", "$_/$language")
+		} @config_dirs;
+	} else {
+		@config_dirs = map {
+			($_, File::Spec->catdir($_, 'default'))
+		} @config_dirs;
+	}
+
+	if($params->{'debug'}) {
+		# Not sure this really does anything
+		# $Config::Auto::Debug = 1;
+
+		if($params->{logger}) {
+			while(my ($key,$value) = each %ENV) {
+				if($value) {
+					$params->{logger}->debug("$key=$value");
 				}
 			}
 		}
+	}
 
-		if(my $lingua = $args{'lingua'}) {
-			my $language;
-			if(($language = $lingua->language_code_alpha2()) && (-d "$config_dir/$language")) {
-				$config_dir .= "/$language";
-			} elsif(-d "$config_dir/default") {
-				$config_dir .= '/default';
-			}
-		}
-	}
-	# if($args{'debug'}) {
-		# # Not sure this really does anything
-		# $Config::Auto::Debug = 1;
-	# }
 	my $config;
-	my $config_file = $args{'config_file'} || $ENV{'CONFIG_FILE'} || File::Spec->catdir($config_dir, $info->domain_name());
-	if($args{logger}) {
-		$args{logger}->debug("Configuration path: $config_file");
-	}
 	eval {
-		if(-r $config_file) {
-			if($args{logger}) {
-				$args{logger}->debug("Found configuration in $config_file");
-			}
-			$config = Config::Auto::parse($config_file);
-		} elsif (-r File::Spec->catdir($config_dir, 'default')) {
-			$config_file = File::Spec->catdir($config_dir, 'default');
-			if($args{logger}) {
-				$args{logger}->debug("Found configuration in $config_file");
-			}
-			$config = Config::Auto::parse('default', path => $config_dir);
-		} else {
-			die "no suitable config file found in $config_dir";
-		}
+		$config = Config::Abstraction->new(
+			config_dirs => \@config_dirs,
+			config_files => ['default', $info->domain_name(), $ENV{'CONFIG_FILE'}, $params->{'config_file'}],
+			logger => $params->{'logger'}
+		)->all();
 	};
 	if($@ || !defined($config)) {
-		throw Error::Simple("$config_file: configuration error: $@");
+		throw Error::Simple("Configuration error: $@");
 	}
 
+	# Validate essential configuration structure
+	throw Error::Simple('Configuration must be a hash reference') unless(ref($config) eq 'HASH');
+
 	# The values in config are defaults which can be overridden by
-	# the values in args{config}
-	if(defined($args{'config'})) {
-		$config = { %{$config}, %{$args{'config'}} };
+	# the values in params->{config}
+	if(defined($params->{'config'})) {
+		$config = { %{$config}, %{$params->{'config'}} };
 	}
 
 	# Allow variables to be overridden by the environment
 	foreach my $key(keys %{$config}) {
-		if($ENV{$key}) {
-			if($args{'logger'}) {
-				$args{'logger'}->debug(__PACKAGE__, ': ', __LINE__, " overwriting $key, ", $config->{$key}, ' with ', $ENV{$key});
+		if(my $value = $ENV{$key}) {
+			# Validate environment variable names
+			# throw Error::Simple("Invalid environment variable name: $key")
+				# unless $key =~ /^[A-Z_][A-Z0-9_]*$/;
+
+			# Sanitize values
+			# $value =~ s/[^\w\s=\.\-]//g;	# Remove potentially dangerous characters
+
+			if($params->{'logger'}) {
+				$params->{'logger'}->debug(__PACKAGE__, ': ', __LINE__, " overwriting $key, ", $config->{$key}, " with $value");
 			}
-			$config->{$key} = $ENV{$key};
+			# If the value contains an equals make it into a hash value
+			if($value =~ /(.+)=(.+)/) {
+				delete $config->{$key} if(!ref($config->{$key}));
+				$config->{$key}{$1} = $2;
+			} else {
+				$config->{$key} = $value;
+			}
 		}
 	}
 
@@ -173,8 +261,11 @@ sub new
 		}
 	}
 
-	unless($config->{'config_path'}) {
-		$config->{'config_path'} = File::Spec->catdir($config_dir, $info->domain_name());
+	# unless($config->{'config_path'}) {
+		# $config->{'config_path'} = File::Spec->catdir($config_dir, $info->domain_name());
+	# }
+	if($params->{'debug'} && $params->{'logger'}) {
+		$params->{'logger'}->debug(__PACKAGE__, '(', __LINE__, '): ', Data::Dumper->new([$config])->Dump());
 	}
 
 	return bless $config, $class;
@@ -187,11 +278,18 @@ sub AUTOLOAD
 
 	return undef unless($self);
 
-	# Extract the method name from the AUTOLOAD variable
+	# Extract the key name from the AUTOLOAD variable
 	(my $key = $AUTOLOAD) =~ s/.*:://;
 
+	# Don't handle special methods
+	return if $key eq 'DESTROY';
+
+	# Validate method name - only allow safe config keys
+	Carp::croak(__PACKAGE__, ": Invalid key name: $key") unless $key =~ /^[a-zA-Z_][a-zA-Z0-9_]*$/;
+
 	# Return the value of the corresponding hash key
-	return $self->{$key};
+	# Only return existing keys to avoid auto-vivification
+	return exists $self->{$key} ? $self->{$key} : undef;
 }
 
 1;
